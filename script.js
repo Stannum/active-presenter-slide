9c7d58ba7de8dd6451eae506b1ca090ace259dca
Stannum <cs.xuyangli@gmail.com>
2021-09-05 00:19:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 const questionList = [
     {
-        subtitle: '产品经理是……？',
-        questions: ['我所在公司对产品经理的定义', '国内产品经理眼花缭乱的职位之间到底有什么区别？'],
+        subtitle: 'It all started when ...',
+        questions: ['What made you become an interviewer?', 
+                    'How would you describe the first few interviews you gave?'
+                   ],
     },
 
     {
-        subtitle: '产品经理的工作日常',
+        subtitle: 'Recruiting talents',
         questions:[
-        '产品经理的一天都在做什么？花在每类事情上的时间分布是？每天总工作时长为多久？', '产品经理最重要的工作技能是什么？', '产品经理最经常使用的工具是什么？']
+        'How to reveal the real capability of the candidates?', 
+            'Could you share some special techniques you would apply, e.g, stress interview?'
+        ]
     },
 
     {
-        subtitle: '产品经理的酸甜苦辣',
-        questions: ['工作里，我最有成就感的事情是……',
-            '工作里，最让我烦恼和头秃的事情是……我最不喜欢的工作内容是……'
+        subtitle: 'Recruiting talents',
+        questions: ['What are the red flags to which you would definitely say no ?'
         ],
     },
      {
-         subtitle:'我们是产品经理，我们都是女孩子',
-         questions: ['所在公司人员性别/种族比例', '女性产品经理的职业天花板',
-         '职业发展道路思考'],
+         subtitle:'Recruiting talents',
+         questions: ['How do you think the probation period could help the overall recruitment process ?', 
+                     'Do you prefer more rounds of interviews plus a short probation period or the other way around?'
+                    ],
     },
     {
-        subtitle:'作为产品经理，我这样点亮技能树',
-        questions: ['非技术出身产品经理对技术该有什么程度的了解', '非技术出身产品经理如何避免成为传话筒', '产品经理如何提升产品能力和专业知识? 怎么发掘自己的技能树并点上技能点？'],
+        subtitle:'Recruiting talents',
+        questions: ['Who from a team will you choose to interview the candidates, in order to get a comprehensive feedback?'],
     },
     {
-        subtitle: '和我一起来做产品经理吧!',
-        questions: ['产品经理所需要的性格特质','如何找到第一份实习; 如何找到一份工作', '作为数据科学家/分析师/程序媛/设计师，如何转行成为产品经理'],
+        subtitle: 'Talking about diversity',
+        questions: ['Have you observed any cultural differences when you interview candidates with different backgrounds?',
+                    'What is your opinion on diversity when recruiting STEM roles?', 
+                    'What strategies will you apply to recruit the minority groups ? Oversampling? Lowering the bar?'],
     },
 ];
 
@@ -41,7 +47,7 @@ const renderQ = function() {
     if (currQuesIdx >= 0 && currQuesIdx < questionList.length) {
         currQues = questionList[currQuesIdx]; 
     } else {
-        currQues = "没有问题啦！"
+        currQues = "It's Q&A time!"
         if (currQuesIdx < 0) {
             currQuesIdx = -1
         } else if (currQuesIdx >= questionList.length) {
