14e74a73f330c1866f9f53ec48d8f91996430b81
Stannum <cs.xuyangli@gmail.com>
2021-09-09 09:51:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ const questionList = [
     
     {
         subtitle: 'It all started when ...',
-        questions: ['1. What made you become an interviewer?', 
-                    '   How would you describe the first few interviews you gave?'
+        questions: ['What made you become an interviewer?', 
+                    'How would you describe the first few interviews you gave?'
                    ],
     },
 
     {
         subtitle: 'Recruiting talents',
-        questions:['2. How to reveal the real capability of the candidates?', 
-            '   Could you share some special techniques you would apply, e.g, stress interview?'
+        questions:['How to reveal the real capability of the candidates?', 
+            'Could you share some special techniques you would apply, e.g, stress interview?'
         ]
     },
 
     {
         subtitle: 'Recruiting talents',
-        questions: ['3. What are the red flags to which you would definitely say no ?'
+        questions: ['What are the red flags to which you would definitely say no ?'
         ],
     },
      {
          subtitle:'Recruiting talents',
-         questions: ['4. How do you think the probation period could help the overall recruitment process ?', 
-                     '   Do you prefer more rounds of interviews plus a short probation period or the other way around?'
+         questions: ['How do you think the probation period could help the overall recruitment process ?', 
+                     'Do you prefer more rounds of interviews plus a short probation period or the other way around?'
                     ],
     },
     {
         subtitle:'Recruiting talents',
-        questions: ['5. Who from a team will you choose to interview the candidates, in order to get a comprehensive feedback?'],
+        questions: ['Who from a team will you choose to interview the candidates, in order to get a comprehensive feedback?'],
     },
     {
         subtitle: 'Talking about diversity',
-        questions: ['6. Have you observed any cultural differences when you interview candidates with different backgrounds?'],
+        questions: ['Have you observed any cultural differences when you interview candidates with different backgrounds?'],
     },
     {
         subtitle: 'Talking about diversity',
-        questions: ['7. What is your opinion on diversity when recruiting STEM roles?', 
-                    '   What strategies will you apply to recruit the minority groups ? Oversampling? Lowering the bar?'],
+        questions: ['What is your opinion on diversity when recruiting STEM roles?', 
+                    'What strategies will you apply to recruit the minority groups ? Oversampling? Lowering the bar?'],
     },
 ];
 
@@ -49,7 +49,7 @@ let currQues = ''
 
 const renderQ = function() {
 
-    $('.q-text ul').empty()
+    $('.q-text ol').empty()
 
     if (currQuesIdx >= 0 && currQuesIdx < questionList.length) {
         currQues = questionList[currQuesIdx]; 
@@ -67,7 +67,7 @@ const renderQ = function() {
     $('.q-text p').text(currQues.subtitle)
 
     currQues.questions.forEach(function(question) {
-        $('.q-text ul').append(`<li>${question}</li>`)
+        $('.q-text ol').append(`<li>${question}</li>`)
     })
 }
 
@@ -89,7 +89,10 @@ const resetQColor = function() {
 
 
 $('.right-nav').on('click', function(e) {
-    e.preventDefault()
+    e.preventDefa
+    
+    
+    t()
     currQuesIdx += 1
     renderQ()
     resetQColor()
